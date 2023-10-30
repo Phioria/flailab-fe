7c253e22e9c18457a6eaadfca19d2373b0b1c1bb
Phioria <chris.baugh@gmail.com>
2023-10-30 11:22:27 -0400

    Update main app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from './components/Register';
+import Login from './components/Login';
+import Admin from './components/Admin';
+import LinkPage from './components/LinkPage';
+import Missing from './components/Missing';
+import Unauthorized from './components/Unauthorized';
+import Home from './components/Home';
+import AllTracks from './components/AllTracks';
+import RequireAuth from './components/RequireAuth';
+import PersistLogin from './components/PersistLogin';
+import ValidateEmail from './components/ValidateEmail';
+
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Upload from './components/Upload';
+import Reset from './components/Reset';
+import ResetLink from './components/ResetLink';
+
+// todo consider not including these codes here so people can't infer info from it
+const ROLES = {
+    User: 1500,
+    Editor: 1999,
+    Admin: 2600,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route element={<PersistLogin />}>
+                    {/* public routes */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="reset" element={<Reset />} />
+                    <Route path="reset/:id/:token" element={<ResetLink />} />
+                    <Route path="verify-email/:id/:token" element={<ValidateEmail />} />
+                    <Route path="linkpage" element={<LinkPage />} />
+                    <Route path="unauthorized" element={<Unauthorized />} />
+
+                    {/* private routes */}
+                    <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+                        <Route path="/records" element={<AllTracks />} />
+                    </Route>
+                    <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}></Route>
+                    <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+                        <Route path="/admin" element={<Admin />} />
+                    </Route>
+                    <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
+                        <Route path="/upload" element={<Upload />} />
+                    </Route>
+                </Route>
+
+                {/* catch all */}
+                <Route path="*" element={<Missing />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
