cddf86f4ea26a97c4ebb136140302c7f0630d6ca
Phioria <chris.baugh@gmail.com>
2023-11-07 14:58:37 -0500

    Change API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //const BASE_URL = 'http://127.0.0.1:5001';
-const BASE_URL = 'https://3.144.196.242:5001';
+const BASE_URL = 'https://ec2-3-144-196-242.us-east-2.compute.amazonaws.com:5001';
 
 export default axios.create({
     baseURL: BASE_URL,
