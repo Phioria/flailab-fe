eba0015db7a659179cc6dee54050a280fd24cb51
Phioria <chris.baugh@gmail.com>
2024-01-09 20:14:39 -0500

    Increase top margin of loading spinner

--- src/components/Register.js
+++ src/components/Register.js
@@ -144,7 +144,7 @@ const Register = () => {
             <Header />
             {isLoading ? (
                 <div
-                    className="d-flex justify-content-center mt-3"
+                    className="d-flex justify-content-center mt-5"
                     aria-label="Spinning DNA molecule representing that tracks are loading"
                 >
                     <img src="/dna.svg" alt="DNA molecule" />
