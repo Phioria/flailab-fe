4268f743a6fa0117517c9ce3252b4ce265cdbe40
Phioria <chris.baugh@gmail.com>
2025-01-20 13:35:26 -0500

    Remove percent_uniquely_mapped from required column list

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -81,7 +81,7 @@ const Upload = () => {
                         <strong>sequencing_type</strong>, <strong>file_location</strong>, <strong>mutant</strong>,{' '}
                         <strong>tissue</strong>, <strong>sex</strong>, <br />
                         <strong>number_mapped</strong>, <strong>library_size</strong>, <strong>percent_aligned</strong>,{' '}
-                        <strong>percent_uniquely_mapped</strong>, <strong>author</strong>, cell_line, <br />
+                        percent_uniquely_mapped, <strong>author</strong>, cell_line, <br />
                         development_stage, project, paper, srr_id, notes, file_name, file_type, <br />
                         paired_single_ended, unmapped_reads, splice_reads, non_splice_reads, <br />
                         reads_mapped_to_plus, reads_mapped_to_minus <br />
